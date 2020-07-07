724186f0f312a82d3d8bbefd7de9fbda40d2b1be
Legenary <yifuluo1@gmail.com>
2020-07-07 12:24:19 -0500

    minor

--- code/src/Whisker.cpp
+++ code/src/Whisker.cpp
@@ -456,13 +456,11 @@ whisker_config Whisker::get_config(std::string wname,Parameters* parameters){
 	whisker_config wc;
     for(int i=0;i<whisker_names.size();i++){
         if(!wname.compare(whisker_names[i])){
-
             wc.id = wname;
             wc.side = whisker_pos[i][0];
             wc.row = whisker_pos[i][1];
             wc.col = whisker_pos[i][2];
 			wc.L = whisker_geom[i][0]; 			// unit: mm
-			std::cout<<"test:"<<wc.L<<std::endl;
             wc.link_angles = whisker_angles[i];
             wc.base_pos = btVector3(whisker_bp_coor[i][0],whisker_bp_coor[i][1],whisker_bp_coor[i][2]);		// unit: mm
             wc.base_rot = btVector3(whisker_bp_angles[i][0]-PI/2,-whisker_bp_angles[i][1],whisker_bp_angles[i][2]+PI/2);
