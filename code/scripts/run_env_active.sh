113a3df6d306d2d884ec0d105573d90bffde9e82
nadinaz <nadinaz@sent.com>
2020-12-02 11:56:58 -0600

    updated environment script

--- code/scripts/run_env_active.sh
+++ code/scripts/run_env_active.sh
@@ -10,8 +10,8 @@
 --ACTIVE 1 \
 --TIME_STOP 1.0 \
 --WHISKER_NAMES R \
---ORIENTATION 0 0 1.570 \
---POSITION -745 40 -140 \
+--ORIENTATION 0.17 0 1.6 \
+--POSITION -95 45 -140 \
 --SAVE_VIDEO 0 \
 --SAVE 0 \
 --file_video "../output/video_wall.mp4" \
